17f80fb09169ab7df73da543744547e737703f09
Kazuya Maekawa <kazifyify@gmail.com>
2016-06-30 19:12:27 +0900

    テスト

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'tweets#index'
   get 'tweets' => 'tweets#index'
   get 'tweets/new' => 'tweets#new'
-  post 'tweets'  =>  'tweets#create'
+  post 'tweets'  =>  'tweets#create' 
 end
